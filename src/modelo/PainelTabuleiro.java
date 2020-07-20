5b8d5249006e3f017a55df5380e0546adf17674d
Asafe Felipe <azapphrz@users.noreply.github.com>
2020-07-20 17:16:49 -0300

    Initial commit

---
+++ src/modelo/PainelTabuleiro.java
@@ -0,0 +1,30 @@
+package modelo;
+
+import java.awt.GridLayout;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel {
+	
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+		
+		setLayout(new GridLayout(
+				tabuleiro.getLinhas(), tabuleiro.getColunas()));
+	
+		tabuleiro.paraCada(c -> add(new BotaoCampo(c)));
+		tabuleiro.registrarObservador(e -> {
+			
+			SwingUtilities.invokeLater(() -> {	
+			if (e.isGanhou())
+				JOptionPane.showMessageDialog(this, "GANHOU!!");
+			else
+				JOptionPane.showMessageDialog(this, "PERDEU!");
+			
+			tabuleiro.reiniciar();
+			});
+		});
+	}
+
+}
---
+++ src/modelo/TelaPrincipal.java
@@ -0,0 +1,24 @@
+package modelo;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class TelaPrincipal extends JFrame {
+
+	private TelaPrincipal() {
+		Tabuleiro tabuleiro = new Tabuleiro(16, 30, 50);
+		PainelTabuleiro painelTabuleiro = new PainelTabuleiro(tabuleiro);
+		
+		add(painelTabuleiro);
+		
+		setTitle("CAMPO MINADO");
+		setSize(690, 438);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new TelaPrincipal();
+	}
+}
